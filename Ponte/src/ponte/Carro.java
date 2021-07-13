375c30f663149b47782066237ccc417e19bc53c9
Kaio Garcia <50223968+kaiofbgarcia@users.noreply.github.com>
2021-07-13 16:58:47 -0300

    Update Carro.java

--- Ponte/src/ponte/Carro.java
+++ Ponte/src/ponte/Carro.java
@@ -4,31 +4,23 @@
  * and open the template in the editor.
  */
 package ponte;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static ponte.Ponte.getPonte;
-import static ponte.Ponte.setPonte;
-
 /**
  *
  * @author kaiof
  */
 public class Carro extends Thread{
+    String nome;
+    Ponte ponte;
     
+    public Carro(String nome, Ponte ponte) {
+        this.nome = nome;
+        this.ponte = ponte;
+    }
+   
     @Override
     public synchronized void run (){  
-        while (getPonte() == true){
-            try {
-                wait();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Carro.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        while(true){
+            ponte.atravessar(nome);
         }
-        setPonte(true);
-        System.out.println(Thread.currentThread().getName() +" está passando");
-        System.out.println("1 2 3 4 5 6 7 8 9 10");
-        setPonte(false);
-        notifyAll();
     }
 }
